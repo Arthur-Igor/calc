a8cc75a8765fb1b56669801a3ac478a378c1d903
Arthur-Igor <arthur15753@gmail.com>
2016-05-19 23:49:07 -0300

    V 1.4
    Foi implementado uma melhoria da reposta ao usuário.
    Foi implementado uma melhoria na organização do código.

--- Calculadora/src/Main.java
+++ Calculadora/src/Main.java
@@ -2,62 +2,72 @@ import java.util.Scanner;
 public class Main {
 //class main - barnche-master
 private static Scanner leia;
-public static void main(String[] args) {
+	
+
+	public static void main(String[] args) {
 leia = new Scanner(System.in);
 //variaveis
-double a1;
-double a2;
-int a=0;
-boolean proximo = true;
+	double a1;
+	double a2;
+	int a=0;
+	boolean proximo = true;
+	String nome = "usu·rio";
 //inicio do bloco de repeti√ß√£o
-while (proximo){
+	while (proximo){
 
-Calculadora calc = new Calculadora("um teste legal");
+		Calculadora calc = new Calculadora(nome);
 
 
-System.out.println("escole um ai \n 1=soma \n 2=subtracao \n 3 = divisao \n 4 = multiplicacao");
-a = leia.nextInt();
-System.out.println("agr escolhe 1 numero");
-a1 = leia.nextDouble();
-System.out.println("agr escolhe 1 numero");
-a2 = leia.nextDouble();
+		System.out.println("escole um ai \n 1=soma \n 2=subtracao \n 3 = divisao \n 4 = multiplicacao \n 5 = potenciacao");
+			a = leia.nextInt();
+		System.out.println("agr escolhe 1 numero");
+			a1 = leia.nextDouble();
+		System.out.println("agr escolhe 1 numero");
+			a2 = leia.nextDouble();
 
 switch(a){
     case 1:
-    	System.out.println(calc.somar(a1,a2));
+    	imprime("soma", calc.somar(a1, a2));
            
             break;
     
     case 2:
 
-    	System.out.println(calc.subtrair(a1, a2));
+    	imprime("subtracao", calc.subtrair(a1, a2));
             break;
     
     case 3:
-        System.out.println(calc.dividir(a1, a2));
+    	imprime("divisao", calc.dividir(a1, a2));
             break;
     
     case 4:
-    	System.out.println(calc.mutiplicar(a1, a2));	
-    	break;
+    	imprime("multiplicacao", calc.mutiplicar(a1, a2));	
+    		break;
+    
     case 5:
-	System.out.println(calc.potencia(a1, a2));
-		break;
+    	imprime("potenciacao", calc.potencia(a1, a2));
+			break;
     	
     	
     		
     default:
-    	System.out.println("qr bagun√ßar?");
+    	System.out.println("OperaÁ„o inv·lida! Tente novamente!");
     	
     	}
-System.out.println("deseja repetir? [s|n]");
-String continua = leia.next();
-if(continua.equals("n") || continua.equals("N"))
-System.out.println("Good bye");
-{
-	proximo = false;
-}
-}
-}
 
+
+
+		System.out.println("deseja repetir? [s|n]");
+		String continua = leia.next();
+			if(continua.equals("n") || continua.equals("N"))
+				System.out.println("Good bye");
+			{
+	proximo = false;
+			}
+		}
+	}
+	public static void imprime(String nomeOper, double resultado) {
+		System.out.println("O resultado da " + nomeOper + " foi: " + resultado);
+		//
+	}
 }
