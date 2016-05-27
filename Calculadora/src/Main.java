bce521b564ea2ba6adcd2d4e074daba855f2f8e7
Arthur-Igor <arthur15753@gmail.com>
2016-05-27 15:52:51 -0300

    1.5
    Adicionado:
    Comentarios implementados.
    Implementação no menu para o usuário.

--- Calculadora/src/Main.java
+++ Calculadora/src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Main {
-//class main - barnche-master
+//class main - branch-master
 private static Scanner leia;
 	
 
@@ -12,20 +12,23 @@ leia = new Scanner(System.in);
 	int a=0;
 	boolean proximo = true;
 	String nome = "usuário";
-//inicio do bloco de repetiÃ§Ã£o
+//inicio do bloco de repeticao
 	while (proximo){
 
 		Calculadora calc = new Calculadora(nome);
 
 
-		System.out.println("escole um ai \n 1=soma \n 2=subtracao \n 3 = divisao \n 4 = multiplicacao \n 5 = potenciacao");
+		System.out.println("Qual operação você quer realizar?\n "
+					+ "1-somar;\n 2-subtrair;\n 3-dividir;\n 4-multiplicar;\n 5-ehPar;\n 6-potencia");
+		//menu
 			a = leia.nextInt();
 		System.out.println("agr escolhe 1 numero");
 			a1 = leia.nextDouble();
 		System.out.println("agr escolhe 1 numero");
 			a2 = leia.nextDouble();
-
-switch(a){
+		//fimMenu
+			//inicio da seleção dos itens do menu
+			switch(a){
     case 1:
     	imprime("soma", calc.somar(a1, a2));
            
@@ -54,9 +57,9 @@ switch(a){
     	System.out.println("Operação inválida! Tente novamente!");
     	
     	}
+			//fimSelecao
 
-
-
+			//selecao para repetir alguma operacao
 		System.out.println("deseja repetir? [s|n]");
 		String continua = leia.next();
 			if(continua.equals("n") || continua.equals("N"))
@@ -64,8 +67,10 @@ switch(a){
 			{
 	proximo = false;
 			}
+			//fimSelecao
 		}
 	}
+	//Implementacao para imprimir o resultado das operacoes
 	public static void imprime(String nomeOper, double resultado) {
 		System.out.println("O resultado da " + nomeOper + " foi: " + resultado);
 		//
