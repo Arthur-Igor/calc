7d3c7669feb32a8c37bcb0e7a81acd7fe95af4f8
Arthur-Igor <arthur15753@gmail.com>
2016-05-27 23:22:48 -0300

    V 1.6
    Adicionado:
    Implementação do laço de repetição;
    Inicio de implementação para verificação dos dados digitados

--- Calculadora/src/Main.java
+++ Calculadora/src/Main.java
@@ -9,26 +9,26 @@ leia = new Scanner(System.in);
 //variaveis
 	double a1;
 	double a2;
-	int a=0;
+	int operacao=0;
 	boolean proximo = true;
 	String nome = "usuário";
 //inicio do bloco de repeticao
-	while (proximo){
+	while (proximo == true){
 
 		Calculadora calc = new Calculadora(nome);
 
 
 		System.out.println("Qual operação você quer realizar?\n "
-					+ "1-somar;\n 2-subtrair;\n 3-dividir;\n 4-multiplicar;\n 5-ehPar;\n 6-potencia");
+					+ "1-somar;\n 2-subtrair;\n 3-dividir;\n 4-multiplicar;\n 5-potencia");
 		//menu
-			a = leia.nextInt();
+			operacao = leia.nextInt();
 		System.out.println("agr escolhe 1 numero");
 			a1 = leia.nextDouble();
 		System.out.println("agr escolhe 1 numero");
 			a2 = leia.nextDouble();
 		//fimMenu
 			//inicio da seleção dos itens do menu
-			switch(a){
+			switch(operacao){
     case 1:
     	imprime("soma", calc.somar(a1, a2));
            
@@ -62,11 +62,11 @@ leia = new Scanner(System.in);
 			//selecao para repetir alguma operacao
 		System.out.println("deseja repetir? [s|n]");
 		String continua = leia.next();
-			if(continua.equals("n") || continua.equals("N"))
+			if(continua.equals("n") || continua.equals("N")){
 				System.out.println("Good bye");
-			{
-	proximo = false;
+				proximo = false;
 			}
+	
 			//fimSelecao
 		}
 	}
@@ -74,5 +74,21 @@ leia = new Scanner(System.in);
 	public static void imprime(String nomeOper, double resultado) {
 		System.out.println("O resultado da " + nomeOper + " foi: " + resultado);
 		//
+	}
+	//Implementaçoa para verificar se o que foi digitado é valido
+	public static double verificaDouble(){
+		boolean naoEDouble = true;
+		double valor = 0;
+		while (naoEDouble) {
+			Scanner leitor = new Scanner(System.in);
+			System.out.println("Digite o valor: ");
+			try {
+				valor = leitor.nextDouble();
+				naoEDouble = false;
+			} catch (Exception e) {
+				System.out.println("Os dados digitados nao são válidos!");
+			}
+		}
+		return valor;
 	}
 }
